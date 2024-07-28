e4305e154dade4739694889d5e4ab2cfa92cb362
Ali <AliHasanJMM@gmail.com>
2024-07-28 16:52:25 +0300

    Accept Loop comments

--- server/acceptLoop.go
+++ server/acceptLoop.go
@@ -4,19 +4,20 @@ import "fmt"
 
 func (s *Server) AcceptLoop() {
 	for {
+		// Accept a new connection
 		conn, err := s.ln.Accept()
 		if err != nil {
 			fmt.Println("Error in connection: ", err)
 			continue
 		}
 
-		// Check if max connections are reached
+		// Check if the maximum number of connections is reached
 		select {
 		case s.activeConnCount <- struct{}{}:
-			// Allow the connection
+			// Allow the connection and start reading from it
 			go s.ReadLoop(conn)
 		default:
-			// Reject the connection
+			// Reject the connection if the server is full
 			conn.Write([]byte("Server is full. Please try again later.\n"))
 			conn.Close()
 		}
