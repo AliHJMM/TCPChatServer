78e71e5ed60babee6c9abec387165061f66c7e77
Ali <AliHasanJMM@gmail.com>
2024-07-28 04:01:14 +0300

    ClientExit Function

--- server/clientExit.go
+++ server/clientExit.go
@@ -6,6 +6,14 @@ import (
 	"time"
 )
 
-
+func (s *Server) ClientExit(conn net.Conn) {
+	s.mu.Lock()
+	name := s.clients[conn]
+	delete(s.clients, conn)
+	s.mu.Unlock()
+	s.msgch <- Message{msg: fmt.Sprintf("%s has left our chat...", name), sender: "System", time: time.Now()}
+	conn.Close()
+	<-s.activeConnCount // Release the connection slot
+}
 
 
