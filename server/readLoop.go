af61de0fb8e284c0390af21637abd996de966975
Ali <AliHasanJMM@gmail.com>
2024-07-28 04:04:48 +0300

    penguin string

--- server/readLoop.go
+++ server/readLoop.go
@@ -6,3 +6,24 @@ import (
 	"strings"
 	"time"
 )
+
+var welcome string = `
+Welcome to TCP-Chat!
+         _nnnn_
+        dGGGGMMb
+       @p~qp~~qMb
+       M|@||@) M|
+       @,----.JM|
+      JS^\__/  qKL
+     dZP        qKRb
+    dZP          qKKb
+   fZP            SMMb
+   HZM            MMMM
+   FqM            MMMM
+ __| ".        |\dS"qML
+ |    '.       | '' \Zq
+_)      \.___.,|     .'
+\____   )MMMMMP|   .'
+     '-'       '--'
+`
+
