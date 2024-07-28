38f267f3b74efcfbd9508dd04d145fabe2317751
Ali <AliHasanJMM@gmail.com>
2024-07-28 16:54:04 +0300

    GetIP Comments

--- server/GetIp.go
+++ server/GetIp.go
@@ -6,13 +6,16 @@ import (
 )
 
 func (s *Server) GetIp() string {
+	// Establish a UDP connection to a public server to determine the local IP
 	conn, err := net.Dial("udp", "8.8.8.8:80")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err) // Log and exit if there's an error
 	}
 	defer conn.Close()
 
+	// Extract the local address and cast it to a UDPAddr
 	localAddress := conn.LocalAddr().(*net.UDPAddr)
 
+	// Return the local IP address as a string
 	return localAddress.IP.String()
 }
