d1c3dd852ec57695933a1472531905bde17d0f8c
Ali <AliHasanJMM@gmail.com>
2024-07-28 04:01:59 +0300

    GetIP Function

--- server/GetIp.go
+++ server/GetIp.go
@@ -5,3 +5,14 @@ import (
 	"net"
 )
 
+func (s *Server) GetIp() string {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddress := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddress.IP.String()
+}
