a550f3cfd52317cce0eb9dfaf6c3b8ff992059da
Ali <AliHasanJMM@gmail.com>
2024-07-28 16:58:20 +0300

    runServer Comments

--- server/runServer.go
+++ server/runServer.go
@@ -3,16 +3,18 @@ package server
 import "net"
 
 
+// RunServer starts the server and listens for incoming TCP connections.
 func (s *Server) RunServer() error {
+	// Start listening on the specified address and port
 	ln, err := net.Listen("tcp", s.listenAddress)
 	if err != nil {
-		return err
+		return err // Return error if the server fails to start
 	}
 
-	defer ln.Close()
+	defer ln.Close() // Ensure the listener is closed when the function exits
 	s.ln = ln
-	go s.AcceptLoop()
-	<-s.ch
-	close(s.msgch)
+	go s.AcceptLoop() // Start accepting connections in a separate goroutine
+	<-s.ch // Wait for a signal on the channel (e.g., for shutdown)
+	close(s.msgch) // Close the message channel after shutting down
 	return nil
 }
