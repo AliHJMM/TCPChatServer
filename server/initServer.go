78a77337a36daa9b47f52ce49186d994c30a3bb7
Ali <AliHasanJMM@gmail.com>
2024-07-28 04:02:56 +0300

    initServer Function

--- server/initServer.go
+++ server/initServer.go
@@ -2,3 +2,13 @@ package server
 
 import "net"
 
+func InitServer(listenAddress string, maxConnections int) *Server {
+	return &Server{
+		listenAddress:   listenAddress,
+		ch:              make(chan struct{}),
+		msgch:           make(chan Message, 10),
+		clients:         make(map[net.Conn]string),
+		history:         make([]Message, 0),
+		activeConnCount: make(chan struct{}, maxConnections),
+	}
+}
