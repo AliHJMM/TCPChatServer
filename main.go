67f40468e2781aa6b6d17b1e49ea986a23a20813
Ali <AliHasanJMM@gmail.com>
2024-07-28 03:58:45 +0300

    main function

--- main.go
+++ main.go
@@ -8,5 +8,18 @@ import (
 )
 
 func main(){
-	
+	var port string
+	args := os.Args
+	if len(args) == 1 {
+		port = ":8989"
+	} else if len(args) == 2 {
+		port = ":" + args[1]
+	} else {
+		fmt.Println("[USAGE]: ./TCPChat $port")
+		os.Exit(0)
+	}
+	server := server.InitServer(port, 10) // Set max connections to 10
+	go server.BroadcastMessages()
+	fmt.Println("Sever Running on: ", server.GetIp(), "And Port", port)
+	log.Fatal(server.RunServer())
 }
