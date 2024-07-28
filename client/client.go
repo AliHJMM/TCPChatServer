66cd609d18f71b1370c517a935f85ec300366eb9
Ali <AliHasanJMM@gmail.com>
2024-07-28 04:09:30 +0300

    main Function

--- client/client.go
+++ client/client.go
@@ -7,6 +7,40 @@ import (
 	"os"
 )
 
+func main() {
+	args := os.Args
+	if len(args) != 3 {
+		fmt.Println("[USAGE]: ./client $port $host")
+		return
+	}
+
+	port := args[1]
+	host := args[2]
+
+	conn, err := net.Dial("tcp", host+":"+port)
+	if err != nil {
+		fmt.Println("Error connecting:", err.Error())
+		return
+	}
+	defer conn.Close()
+
+	reader := bufio.NewReader(conn)
+	fmt.Print(readMessage(reader))
+
+	go readServerMessages(reader)
+
+	writer := bufio.NewWriter(conn)
+	for {
+		fmt.Print("[YOU]: ")
+		message, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		if message == "\n" {
+			continue
+		}
+		sendMessage(writer, message)
+	}
+}
+
+
 func readMessage(reader *bufio.Reader) string {
 	message, _ := reader.ReadString('\n')
 	return message
