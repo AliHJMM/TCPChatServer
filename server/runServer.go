8edf0e10b9713d1541d0b202ff4a8a69705f98e7
Ali <AliHasanJMM@gmail.com>
2024-07-28 04:06:11 +0300

    runServer Function

--- server/runServer.go
+++ server/runServer.go
@@ -3,3 +3,16 @@ package server
 import "net"
 
 
+func (s *Server) RunServer() error {
+	ln, err := net.Listen("tcp", s.listenAddress)
+	if err != nil {
+		return err
+	}
+
+	defer ln.Close()
+	s.ln = ln
+	go s.AcceptLoop()
+	<-s.ch
+	close(s.msgch)
+	return nil
+}
