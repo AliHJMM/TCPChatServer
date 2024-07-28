5d0d01bd4a012c601353d8cee7870a8785f5a715
Ali <AliHasanJMM@gmail.com>
2024-07-28 16:53:24 +0300

    Client Exit Comments

--- server/clientExit.go
+++ server/clientExit.go
@@ -7,13 +7,16 @@ import (
 )
 
 func (s *Server) ClientExit(conn net.Conn) {
+	// Lock to safely access the clients map
 	s.mu.Lock()
-	name := s.clients[conn]
-	delete(s.clients, conn)
+	name := s.clients[conn] // Get the client's name
+	delete(s.clients, conn)  // Remove the client from the map
 	s.mu.Unlock()
+
+	// Notify all clients that this client has left
 	s.msgch <- Message{msg: fmt.Sprintf("%s has left our chat...", name), sender: "System", time: time.Now()}
-	conn.Close()
-	<-s.activeConnCount // Release the connection slot
+	conn.Close() // Close the connection
+	<-s.activeConnCount// Decrement the active connection count
 }
 
 
