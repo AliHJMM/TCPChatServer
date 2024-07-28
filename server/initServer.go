6c7d15c27c7fae128c7f562b59c3c6bb1f463413
Ali <AliHasanJMM@gmail.com>
2024-07-28 16:55:01 +0300

    initServer Comments

--- server/initServer.go
+++ server/initServer.go
@@ -2,13 +2,14 @@ package server
 
 import "net"
 
+// InitServer initializes a new server with the given listening address and maximum connections.
 func InitServer(listenAddress string, maxConnections int) *Server {
 	return &Server{
-		listenAddress:   listenAddress,
-		ch:              make(chan struct{}),
-		msgch:           make(chan Message, 10),
-		clients:         make(map[net.Conn]string),
-		history:         make([]Message, 0),
-		activeConnCount: make(chan struct{}, maxConnections),
+		listenAddress:   listenAddress, // Address on which the server listens
+		ch:              make(chan struct{}), // General-purpose signal channel
+		msgch:           make(chan Message, 10),  // Channel for broadcasting messages, buffered with a size of 10
+		clients:         make(map[net.Conn]string),  // Map of active clients, with connection as key and username as value
+		history:         make([]Message, 0),   // Slice to store chat history
+		activeConnCount: make(chan struct{}, maxConnections), // Channel to limit the number of active connections
 	}
 }
