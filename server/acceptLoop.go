3b142d7b75bfd606dc7bc5cf774864b3d28a25e5
Ali <AliHasanJMM@gmail.com>
2024-07-28 04:00:23 +0300

    AcceptLoop Function

--- server/acceptLoop.go
+++ server/acceptLoop.go
@@ -2,4 +2,23 @@ package server
 
 import "fmt"
 
+func (s *Server) AcceptLoop() {
+	for {
+		conn, err := s.ln.Accept()
+		if err != nil {
+			fmt.Println("Error in connection: ", err)
+			continue
+		}
 
+		// Check if max connections are reached
+		select {
+		case s.activeConnCount <- struct{}{}:
+			// Allow the connection
+			go s.ReadLoop(conn)
+		default:
+			// Reject the connection
+			conn.Write([]byte("Server is full. Please try again later.\n"))
+			conn.Close()
+		}
+	}
+}
